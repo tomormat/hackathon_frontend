2048665f206d9eab671c35f874749ad1ff181759
tomormat <tomormat@gmail.com>
2025-08-01 20:50:02 +0200

    HRML Mockup for Personal, History, routing for /

--- frontend_src/src/app/app.routes.ts
+++ frontend_src/src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { Personal } from './pages/personal/personal';
 import { History } from './pages/history/history';
-import { App } from './app';
+import { Home } from './pages/home/home';
 
 export const routes: Routes = [
     {path: 'personal', component: Personal},
     {path: 'history', component: History},
+    {path: '', component: Home, pathMatch: 'full'},
 ];
