aa6c7e1efcc44969cf0f3199f42d176efdc9d277
tomormat <tomormat@gmail.com>
2025-08-01 20:30:39 +0200

    Made the basic routes for personal and history pages, added their components, and created basic HTML and CSS files for them.

--- frontend_src/src/app/app.routes.ts
+++ frontend_src/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Personal } from './pages/personal/personal';
+import { History } from './pages/history/history';
+import { App } from './app';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'personal', component: Personal},
+    {path: 'history', component: History},
+];
