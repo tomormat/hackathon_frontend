63f5e3b93270061b1aa30b8ed78846059b0580e2
Heather Thomson <heather.eigg@hotmail.co.uk>
2025-08-04 15:29:33 +0100

    Add in routing in personal and history pages

--- frontend_src/src/app/services/stock.service.ts
+++ frontend_src/src/app/services/stock.service.ts
@@ -7,9 +7,18 @@ import { Observable } from 'rxjs';
 })
 export class StockService {
   private mockStocksUrl = 'assets/mock-shares.json'; // tell it where the database is 
-  constructor(private http: HttpClient) {}
+  private stocks: any[] = [];
+
+  constructor(private http: HttpClient) {
+    this.http.get<any[]>(this.mockStocksUrl).subscribe(data => {
+        this.stocks = data;
+      });
+  }
 
   getStocks(): Observable<any[]> {
     return this.http.get<any[]>(this.mockStocksUrl);
+  }
+  getStockById(id: number): any {
+    return this.stocks.find(stock => stock.id === id);
   }
 }
