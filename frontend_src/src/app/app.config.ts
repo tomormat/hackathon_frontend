cce1ff688e55f4f2aa9992ec97cbe84732de5a9f
Heather Thomson <heather.eigg@hotmail.co.uk>
2025-08-04 08:33:57 +0100

    Set up inital containers for Landing page

--- frontend_src/src/app/app.config.ts
+++ frontend_src/src/app/app.config.ts
@@ -8,6 +8,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes), 
+    provideClientHydration(withEventReplay())
   ]
 };
