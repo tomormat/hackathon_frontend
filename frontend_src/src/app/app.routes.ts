79978f4e790656c641568182652c3b0d359f6dab
Heather Thomson <heather.eigg@hotmail.co.uk>
2025-08-07 09:28:33 +0100

    get main matching dev

--- frontend_src/src/app/app.routes.ts
+++ frontend_src/src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
 import { Personal } from './pages/personal/personal';
 import { History } from './pages/history/history';
-import { App } from './app';
+import { Home } from './pages/home/home';
 
 export const routes: Routes = [
     {path: '', component: Home, pathMatch: 'full'},
     {path: 'personal/:id', component: Personal}, // seperate personal page for each stock (indexed by id)
     {path: 'history', component: History},
+    
 ];
